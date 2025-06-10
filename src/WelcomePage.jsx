9b2caa983f8768470a56364042a3487e8ed5e5a0
Alia Taher <alia.taher022@gmail.com>
2025-06-10 23:17:30 +0200

    Add WelcomePage component for the Halal Food Awareness Quiz

---
+++ src/WelcomePage.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <p className="title">Halal Food Awareness Quiz</p>
+
+      <Button text="Start Game" />
+    </div>
+  );
+};
+
+export default WelcomePage;
